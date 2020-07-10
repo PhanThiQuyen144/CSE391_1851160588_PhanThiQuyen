6cf4bc188e081d1d4d50ec412afde2605abe1951
PhanThiQuyen144 <67349994+PhanThiQuyen144@users.noreply.github.com>
2020-07-10 16:54:56 +0700

    Delete bai1.11.js

--- HOMEWORK_01/bai1.15/bai1.11.js
+++
@@ -1,5 +0,0 @@
-// alert('thong bao');
-function myFuntion() {
-    // document.getElementById('demo').innerText= "ptnhung";
-    document.getElementById('demo').innerText= document.getElementById('manguoidung').value;
-}
