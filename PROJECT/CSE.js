5061f913ff153abeb39f56ad870d78ceb22b9b6e
KimCuc273 <67349961+KimCuc273@users.noreply.github.com>
2020-07-30 20:35:11 +0700

    CSE

---
+++ PROJECT/CSE.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $(".menu li").hover(function(){
+//         $(this).find("ul:first").slideDown(500);
+//     },function(){
+//         $(this).find("ul:first").hide(300);
+
+//     });
+// });
